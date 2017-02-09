a32a1fc39b2b82830b57a45b22f8b0cfec0b65bc
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 06:46:28 -0500

    create Todo component

---
+++ src/components/todos/Todo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+
+  render() {
+    return (
+      <li>{this.props.todo.text}</li>
+    );
+  };
+};
+
+export default Todo
