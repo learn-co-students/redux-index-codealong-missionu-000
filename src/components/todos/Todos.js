357858ed18ba9691cf4fedf93a7b3113ff199951
Luke Ghenco <lukeghenco@gmail.com>
2017-02-09 06:46:48 -0500

    add Todo component injection and fix syntax

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Todo from './Todo';
 
 class Todos extends Component {
 
-  render(){
-    let todos = this.props.store.getState().todos.map(function(todo){
-      return <li> {todo.text} </li>
-    })
-    return(
+  render() {
+    const todos = this.props.store.getState().todos.map((todo, index) => {
+      return <Todo key={index} todo={todo} />
+    });
+
+    return (
       <ul>
         {todos}
       </ul>
-    )
-  }
-}
+    );
+  };
+};
 
 export default Todos;
