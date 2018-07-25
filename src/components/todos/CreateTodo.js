fd9cb9e15828d950f2fc8e4ffa16821f7ca83829
maxwell <github email address>
2018-07-25 08:05:14 -0400

    udpated solution

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,6 +13,7 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    this.setState({text: ''})
   }
 
   handleChange(event) {
